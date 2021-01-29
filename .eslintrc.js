14f097d1c9ca572f3e45e46525c5da18b7c17f07
peteragnus <prabhuchira@gmail.com>
2021-01-29 17:31:19 +0530

    added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
+// module.exports = {
+//   root: true,
+//   extends: '@react-native-community',
+// };
+
 module.exports = {
-  root: true,
+  // ...
+  // root:true,
   extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': 0,
+  },
+  // ...
 };
